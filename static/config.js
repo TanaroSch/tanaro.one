2fcb0ddadcd8451655be3a824a709f8aeab01a5a
Tanaro Schädler <tanaro.schaedler@uni-ulm.de>
2024-07-07 00:22:00 +0200

    add config to header

---
+++ static/config.js
@@ -0,0 +1,42 @@
+// config.js
+const headerConfig = {
+    "branding": {
+        "text": "Tanaro"
+    },
+    "navigation": [
+        {
+            "text": "Home",
+            "link": "#home"
+        },
+        {
+            "text": "About",
+            "link": "#about"
+        },
+        {
+            "text": "Services",
+            "link": "#services",
+            "dropdown": [
+                {
+                    "text": "Web Design",
+                    "link": "#web-design"
+                },
+                {
+                    "text": "App Development",
+                    "link": "#app-development"
+                },
+                {
+                    "text": "SEO Optimization",
+                    "link": "#seo"
+                },
+                {
+                    "text": "IT Consulting",
+                    "link": "#consulting"
+                }
+            ]
+        },
+        {
+            "text": "Contact",
+            "link": "#contact"
+        }
+    ]
+};
